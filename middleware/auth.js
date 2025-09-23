16780faef2d04ac087597e196b437ec544a21f67
Nikhil Singh <95328684+nikhil-singh1@users.noreply.github.com>
2025-09-23 13:44:49 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,9 +7,11 @@ module.exports = async function(req, res, next) {
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = { id: decoded.id }; // must match author id type
+    // req.user = { id: decoded.id }; // must match author id type
+    req.user = decoded.user; 
     next();
   } catch (err) {
     res.status(401).json({ msg: 'Token is not valid' });
   }
 };
+
