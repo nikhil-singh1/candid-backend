3d47937aaebabb1ec493b3dba4be597a948a1566
Nikhil Singh <95328684+nikhil-singh1@users.noreply.github.com>
2025-09-23 09:28:51 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,42 @@
+// const mongoose = require('mongoose');
+// const connectDB = async () => {
+// try {
+// await mongoose.connect(process.env.MONGO_URI, {
+// useNewUrlParser: true,
+// useUnifiedTopology: true,
+// });
+// console.log('MongoDB connected');
+// } catch (err) {
+// console.error(err);
+// process.exit(1);
+// }
+// };
+
+// module.exports = connectDB;
+
+
 const mongoose = require('mongoose');
-const connectDB = async () => {
-try {
-await mongoose.connect(process.env.MONGO_URI, {
-useNewUrlParser: true,
-useUnifiedTopology: true,
-});
-console.log('MongoDB connected');
-} catch (err) {
-console.error(err);
-process.exit(1);
+
+let cached = global.mongoose;
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
 }
-};
+
+async function connectDB() {
+  if (cached.conn) return cached.conn;
+
+  if (!cached.promise) {
+    const opts = {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    };
+    cached.promise = mongoose.connect(process.env.MONGO_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
+}
+
 module.exports = connectDB;
